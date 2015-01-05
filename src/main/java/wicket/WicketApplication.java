0ee455193e71346d18fd84d5bb81796b00595e07
Ashot <ashot2005@yahoo.com>
2015-01-05 23:11:27 +0400

    Initial commit

---
+++ src/main/java/wicket/WicketApplication.java
@@ -0,0 +1,26 @@
+package wicket;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ */
+public class WicketApplication extends WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+	}
+	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	public Class getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
